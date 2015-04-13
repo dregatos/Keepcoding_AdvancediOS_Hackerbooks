bd922055917597b88a3567e31ce6fc7c44e198b6
David Regatos <david.regatos@gmail.com>
2015-04-13 16:33:44 +0200

    Initial Commit

---
+++ HackerBooks2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HackerBooks2
+//
+//  Created by David Regatos on 13/04/15.
+//  Copyright (c) 2015 DRG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
