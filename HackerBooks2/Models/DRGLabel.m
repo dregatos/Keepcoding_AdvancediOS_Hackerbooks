f37c36917f775bc390eda8f3582d874a2c6ee521
David Regatos <david.regatos@gmail.com>
2015-04-16 16:07:34 +0200

    avoid duplicated labels and writers on the database;

--- HackerBooks2/Models/DRGLabel.m
+++ HackerBooks2/Models/DRGLabel.m
@@ -12,8 +12,23 @@
 
 + (instancetype)labelNamed:(NSString *)name withContext:(NSManagedObjectContext *)context {
     
-    DRGLabel *label = [DRGLabel insertInManagedObjectContext:context];
-    label.name = name;
+    // Check if it already exists
+    NSFetchRequest *req = [NSFetchRequest fetchRequestWithEntityName:[[self class] entityName]];
+    req.predicate = [NSPredicate predicateWithFormat:@"name MATCHES[cd] %@",name];
+    
+    NSArray *results = [context executeFetchRequest:req error:nil];
+    
+    DRGLabel *label;
+    if ([results count] == 0) {
+        // Create a new one
+        label = [DRGLabel insertInManagedObjectContext:context];
+        label.name = name;
+    } else if ([results count] == 1) {
+        // Use previous one
+        label = [results lastObject];
+    } else if ([results count] > 1) {
+        NSLog(@"Duplicated!! Label \'%@\' was found %lu times on our DB", name, [results count]);
+    }
     
     return label;
 }
--- HackerBooks2/Models/DRGWriter.m
+++ HackerBooks2/Models/DRGWriter.m
@@ -12,8 +12,21 @@
 
 + (instancetype)writerNamed:(NSString *)name withContext:(NSManagedObjectContext *)context {
     
-    DRGWriter *writer = [DRGWriter insertInManagedObjectContext:context];
-    writer.name = name;
+    // Check if writer exists
+    NSFetchRequest *req = [NSFetchRequest fetchRequestWithEntityName:[[self class] entityName]];
+    req.predicate = [NSPredicate predicateWithFormat:@"name MATCHES[cd] %@",name];
+
+    NSArray *results = [context executeFetchRequest:req error:nil];
+    
+    DRGWriter *writer;
+    if ([results count] == 0) {
+        writer = [DRGWriter insertInManagedObjectContext:context];
+        writer.name = name;
+    } else if ([results count] == 1) {
+        writer = [results lastObject];
+    } else if ([results count] > 1) {
+        NSLog(@"Duplicated!! Writer \'%@\' was found %lu times on our DB", name, [results count]);
+    }
     
     return writer;
 }
